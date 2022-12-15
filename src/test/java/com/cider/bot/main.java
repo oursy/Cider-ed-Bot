a04c25d31868cb50f92d38ba5ec151610605fa83
oursy <awaylvs@gmail.com>
2022-12-16 02:11:33 +0800

    Update main.java

--- src/test/java/com/cider/bot/main.java
+++ src/test/java/com/cider/bot/main.java
@@ -8,7 +8,6 @@ import org.web3j.utils.Convert.Unit;
 public class main {
 
   public static void main(String[] args) {
-    System.out.println(bigDecimal.toPlainString());
     System.out.println("GearSold:"+Hash.sha3String("GearSold(address,uint256,uint256)"));
     System.out.println("GearBought:"+Hash.sha3String("GearBought(address,uint256)"));
     System.out.println("LPClaimed:"+Hash.sha3String("LPClaimed(address,uint256)"));
